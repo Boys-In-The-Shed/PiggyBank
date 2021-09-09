3d629f94d047c82c83d3334c556fabb2ea7be1d6
benjamin-hoggan <ben_g-man@windowslive.com>
2021-09-09 18:22:43 +1200

    Added POST /payment/setup

--- src/pb-app/src/checkout-form/index.jsx
+++ src/pb-app/src/checkout-form/index.jsx
@@ -1,31 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {ElementsConsumer, CardElement} from '@stripe/react-stripe-js';
 
 import CardSection from '../card-section/index.jsx';
 import ButtonBase from '../button-base/index.jsx';
 import './checkout-form.css';
 
-class CheckoutForm extends React.Component {
+async function paymentSetup(amount) {
+  const response = await fetch('https://api.piggybank.lukejoshuapark.io/payment/setup', {
+    method: 'POST',
+    mode: 'cors', 
+    headers: {
+      'content-type': 'application/json'
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify({amount: parseFloat(amount)})
+  });
+  const responseModel =  await response.json();
+  if (response.status !== 200 && !responseModel.error) {
+    responseModel.error = "Internal Server Error :(";
+  }
+  return responseModel;
+}
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      dollarAmount: ""
-    };
+const CheckoutForm = ({ onResult, stripe, elements }) => {
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     dollarAmount: ""
+  //   };
+    // this.onResult = this.props.onResult.bind(this);
+  // }
+  const [dollarAmount, setDollarAmount] = useState("");
+  const [formMessage, setFormMessage] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+  const [paymentIntentID, setPaymentIntentID] = useState("");
+
+  const submitClick = async (amount) => {
+    setFormMessage("");
+    if (dollarAmount == "" ) {
+      setFormMessage("Please enter some money!");
+    }
+    let responseModel = await paymentSetup(amount)
+    if (responseModel.error) {
+      setFormMessage(responseModel.error);
+      return;
+    }
+    setClientSecret(responseModel.client_secret);
+    setPaymentIntentID(responseModel.payment_intent_id);
+    // setFormMessage(responseModel.client_secret + " + " + responseModel.payment_intent_id)
+
+    // TODO add handleSubmit function call
   }
 
-  handleSubmit = async (dollarAmount) => {
-    const {stripe, elements} = this.props;
+  const handleSubmit = async (dollarAmount) => {
 
     if (!stripe || !elements) {
       return;
     }
 
+    // Change {CLIENT_SECRET}
     const result = await stripe.confirmCardPayment('{CLIENT_SECRET}', {
       payment_method: {
         card: elements.getElement(CardElement),
         billing_details: {
-          name: 'Jenny Rosen',
+          name: 'Lukey Parky',
         },
       }
     });
@@ -36,6 +76,9 @@ class CheckoutForm extends React.Component {
     } else {
       // The payment has been processed!
       if (result.paymentIntent.status === 'succeeded') {
+        setFormMessage("Successful!");
+        // TODO POST successful payment to server
+        
         // Show a success message to your customer
         // There's a risk of the customer closing the window before callback
         // execution. Set up a webhook or plugin to listen for the
@@ -45,18 +88,21 @@ class CheckoutForm extends React.Component {
     }
   };
 
-  render() {
-    return (
-      <form>
-        <div className="checkoutForm">
-          <div>$</div>
-          <input type="number" onChange={e => this.setState({ dollarAmount: e.target.value })}></input>
-        </div>
-        <CardSection />
-        <ButtonBase userClick={() => this.handleSubmit(this.state.dollarAmount)}>PAY US!</ButtonBase>
-      </form>
-    );
-  }
+  
+
+  return (
+    <form>
+      <div className="checkoutForm">
+        <div>$</div>
+        <input type="number" onChange={e => setDollarAmount(e.target.value)}></input>
+      </div>
+      <CardSection />
+      <div>
+        <div className='form-message'>{formMessage}</div>
+        <ButtonBase userClick={() => submitClick(dollarAmount)}>PAY US!</ButtonBase>
+      </div>
+    </form>
+  );
 }
 
 export default function InjectedCheckoutForm() {
